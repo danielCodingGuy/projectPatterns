e35b676a55bae768e4f2da817c103544a5f97e0e
Daniel <102466020+danielCodingGuy@users.noreply.github.com>
2022-03-27 13:17:14 +0200

    Added Composite
    Simple Composite project pattern made in Java

---
+++ Composite/Demo.java
@@ -0,0 +1,32 @@
+package Kompozyt;
+
+import Kompozyt.ImageEditor;
+import Kompozyt.Circle;
+import Kompozyt.CompoundShape;
+import Kompozyt.Dot;
+import Kompozyt.Rectangle;
+
+import java.awt.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
