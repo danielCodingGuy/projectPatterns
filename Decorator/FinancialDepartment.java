4f2b51aca6ff638c5e5bed717833843367354b95
Daniel <102466020+danielCodingGuy@users.noreply.github.com>
2022-03-27 13:18:35 +0200

    Added Decorator

---
+++ Decorator/FinancialDepartment.java
@@ -0,0 +1,9 @@
+public class FinancialDepartment implements Department {
+
+    private Integer id;
+    private String name;
+
+    public void printDepartmentName() {
+        System.out.println(getClass().getSimpleName());
+    }
+}
---
+++ Decorator/SalesDepartment.java
@@ -0,0 +1,9 @@
+public class SalesDepartment implements Department {
+
+    private Integer id;
+    private String name;
+
+    public void printDepartmentName() {
+        System.out.println(getClass().getSimpleName());
+    }
+}
